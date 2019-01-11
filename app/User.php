2116bb2d22413f45377691cbf4ddedadfe52ea95
marko.zugic <markozugic4@gmail.com>
2019-01-11 16:12:14 +0100

    added auth

--- app/User.php
+++ app/User.php
@@ -31,4 +31,24 @@ class User extends Authenticatable
     public function bills() {
         $this->hasMany(App\Bill::class);
     }
+    
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
